d30039303c03f3d3f2b442b23b8fa546ca6e07a2
Alejandro Rivera <alejo@lapix.com.co>
2026-01-28 18:35:22 -0500

    bump versions

--- tests/CreateTokenTest.php
+++ tests/CreateTokenTest.php
@@ -247,7 +247,11 @@ class CreateTokenTest extends TestCase
             ['', InvalidJSONWebToken::class],
             ['qwerty.qwerty.qwerty', InvalidJSONWebToken::class],
             ['qwerty.qwerty', InvalidJSONWebToken::class],
-            [JWT::encode(['sub' => 'me'], 'qwerty', 'HS256'), InvalidJSONWebToken::class],
+            [JWT::encode(
+                payload: ['sub' => 'me'],
+                key: 'qwerty123-qwerty123-qwerty123-qwerty123',
+                alg: 'HS256',
+            ), InvalidJSONWebToken::class],
             [
                 JWT::encode(['sub' => 'me'], $this->newEdDSAKey()->getPrivateKey(), 'EdDSA', '1'),
                 InvalidJSONWebToken::class,
